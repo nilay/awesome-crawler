ea77b441ca2bf355c0143ddb9695a7655c6cd9ee
Nilay <nilayanand@gmail.com>
2017-04-06 18:34:54 +0530

    Implement crawl as background task

--- app/workers/crawl_worker.rb
+++ app/workers/crawl_worker.rb
@@ -3,7 +3,7 @@ class CrawlWorker
   def perform(website_id)
     website = Website.find(website_id)
     website.crawl_started_at =  DateTime.now
-    website.content =  Crawler.crawl(website.url)
+    website.content =  AwesomeCrawler.crawl(website.url)
     website.crawl_completed_at =  DateTime.now
     website.status = 'done'
     website.save
--- lib/awesome_crawler.rb
+++ lib/awesome_crawler.rb
@@ -1,7 +1,16 @@
 class AwesomeCrawler
   def self.crawl(url)
     response = HTTParty.get(url)
-    response.code == 200 ? response.body : nil
+    if response.code == 200
+      html_doc = Nokogiri::HTML(response.body)
+
+      h1 = html_doc.xpath("//h1")
+      h2 = html_doc.xpath("//h2")
+      h3 = html_doc.xpath("//h3")
+      anchor = html_doc.xpath("//a")
+      [h1, h2, h3, anchor].join(' ')
+
+    end
 
   end
 end
